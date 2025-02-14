1614bd7c90eaaf8335d59cdfc2f28954375675d1
AhmadMujtaba200210 <cadeta6@gmail.com>
2025-02-14 16:20:30 -0500

    initial commit

---
+++ src/test/java/org/engicodes/contentservice/ContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.engicodes.contentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
